5b8291cbda1eca5cd781515e0290a4f43943d668
fmacedosantos <felipemacedo986@gmail.com>
2025-05-17 07:33:39 -0300

    Inserting user

--- src/main/java/com/fmacedosantos/blog/services/UserService.java
+++ src/main/java/com/fmacedosantos/blog/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fmacedosantos.blog.services;
 
 import com.fmacedosantos.blog.domain.User;
+import com.fmacedosantos.blog.dto.UserDTO;
 import com.fmacedosantos.blog.repository.UserRepository;
 import com.fmacedosantos.blog.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
