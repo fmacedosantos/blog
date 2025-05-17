80cd981b04243d44b85d0c0442d416249d2b1a4e
fmacedosantos <felipemacedo986@gmail.com>
2025-05-17 07:50:32 -0300

    Updating user

--- src/main/java/com/fmacedosantos/blog/resources/UserResource.java
+++ src/main/java/com/fmacedosantos/blog/resources/UserResource.java
@@ -50,4 +50,14 @@ public class UserResource {
 
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+
+        service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
 }
