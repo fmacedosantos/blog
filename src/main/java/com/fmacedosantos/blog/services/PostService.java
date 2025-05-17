c0d36685d4f667c1ecc2ec0c520f45a4f95dc653
fmacedosantos <felipemacedo986@gmail.com>
2025-05-17 09:42:16 -0300

    Getting a post by id

---
+++ src/main/java/com/fmacedosantos/blog/services/PostService.java
@@ -0,0 +1,22 @@
+package com.fmacedosantos.blog.services;
+
+import com.fmacedosantos.blog.domain.Post;
+import com.fmacedosantos.blog.repository.PostRepository;
+import com.fmacedosantos.blog.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
